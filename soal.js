854d5c1df7a803f925c1608b98c6660a2a8e3f52
FebrianAditya <febrian.aditya@student.upi.edu>
2025-05-17 07:49:08 +0700

    soal latihan

---
+++ soal.js
@@ -0,0 +1,26 @@
+// SOAL 1 Luas segitiga 
+let alasSegitiga = 10;
+let tinggiSegitiga = 8;
+let resultLuasSegiTiga
+
+// SOAL 2 hitung panjang greetingWord 
+let greetingWord = "Selamat Pagi Peserta ODP BNI"
+let resultLength
+
+// SOAL 3 Konversi menit ke JAM:MENIT
+// Contoh 135 = 2:15
+let extraTime = 135
+let resultConversion
+
+// SOAL 4 Ambil Digit Puluhan dan Satuan dari Angka 2 Digit
+let angka = 95; // puluhan = 9, satuan = 5
+let resultConversionNumberDigit
+
+// SOAL 5 Hasil true jika minimal ada 2 angka yang sama
+let x1 = 5, y1 = 5, z1 = 7;
+let x2 = 5, y2 = 5, z2 = 5;
+let x3 = 1, y3 = 2, z3 = 3;
+
+let resultOne // TRUE
+let resultTwo // TRUE
+let resultThree // FALSE
